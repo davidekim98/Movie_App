a085d848acb96611ff458669b7aa05cf057b174d
dmlwns**** <dmlwns9898@naver.com>
2021-01-23 14:20:35 +0000

    first commit

---
+++ client/src/components/Config.js
@@ -0,0 +1,8 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = "https://api.themoviedb.org/3/";
+export const API_KEY = "7b8130ffeb52a6a150ca0612cded1154";
+
+export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+
---
+++ client/src/components/views/commons/GridCards.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Col } from 'antd';
+
+function GridCards() {
+	return (
+		<div>
+			<Col lg={6} md={8} sm={24}>
+				<div style={{ position: 'relative' }}>
+					<a href>
+						<img />
+					</a>
+				</div>
+			</Col>
+		</div>
+	)
+}
+
+export default GridCards
